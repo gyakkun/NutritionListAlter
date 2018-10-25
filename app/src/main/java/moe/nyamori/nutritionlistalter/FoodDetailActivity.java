d212e5ee62a09ed406218ea098ede3b3d11cc74f
gyakkun <xiaowufeixia@gmail.com>
2018-10-25 17:03:54 +0800

    Seperating two holder and adapter.

--- app/src/main/java/moe/nyamori/nutritionlistalter/FoodDetailActivity.java
+++ app/src/main/java/moe/nyamori/nutritionlistalter/FoodDetailActivity.java
@@ -2,19 +2,33 @@ package moe.nyamori.nutritionlistalter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.UUID;
 
 public class FoodDetailActivity extends AppCompatActivity {
 
     private static final String EXTRA_FOOD_NAME = "moe.nyamori.nutritionlistalter.foodname";
-    private static final String ARG_FOOD_NAME = "food_name";
     private Food mFood;
+    private RelativeLayout mDetailTop;
+    private TextView mDetailFoodName;
+    private TextView mDetailFoodCat;
+    private TextView mDetailFoodNutrition;
+    private ImageButton mDetailStar;
+    private ImageButton mDetailCart;
 
 
-    public FoodDetailActivity newInstance(String foodName){
+    public FoodDetailActivity newInstance(String foodName) {
         mFood = FoodStore.get(FoodDetailActivity.this).getFood(foodName);
         FoodDetailActivity activity = new FoodDetailActivity();
 
@@ -25,9 +39,48 @@ public class FoodDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+
+        mDetailTop = (RelativeLayout) findViewById(R.id.detail_top);
+        mDetailFoodName = (TextView) findViewById(R.id.detail_food_name);
+        mDetailFoodCat = (TextView) findViewById(R.id.detail_food_cat);
+        mDetailFoodNutrition = (TextView) findViewById(R.id.detail_food_nutrition);
+        mDetailStar = (ImageButton) findViewById(R.id.detail_star_button);
+        mDetailCart = (ImageButton) findViewById(R.id.detail_shoppling_cart);
+
+        String foodName = getIntent().getStringExtra(EXTRA_FOOD_NAME);
+        mFood = FoodStore.get(getApplicationContext()).getFood(foodName);
+
+        mDetailTop.setBackgroundColor(Color.parseColor(mFood.getBgColor()));
+        mDetailFoodName.setText(mFood.getName());
+        mDetailFoodCat.setText(mFood.getCat());
+        mDetailFoodNutrition.setText("富含 " + mFood.getNutrition());
+
+        if (mFood.isStar()) {
+            mDetailStar.setImageResource(R.drawable.ic_action_starred);
+        } else {
+            mDetailStar.setImageResource(R.drawable.ic_action_star_empty);
+        }
+
+        mDetailCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (FoodListActivity.addFoodToCart(mFood.getName())) {
+                    Toast.makeText(getApplicationContext(),
+                            mFood.getName() + " 已经加入购物车",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            mFood.getName() + " 已在购物车，不能重复添加",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
-    public static Intent newIntent(Context packageContext, String foodName){
+
+    public static Intent newIntent(Context packageContext, String foodName) {
         Intent intent = new Intent(packageContext, FoodDetailActivity.class);
         intent.putExtra(EXTRA_FOOD_NAME, foodName);
         return intent;
--- app/src/main/java/moe/nyamori/nutritionlistalter/FoodListActivity.java
+++ app/src/main/java/moe/nyamori/nutritionlistalter/FoodListActivity.java
@@ -1,35 +1,83 @@
 package moe.nyamori.nutritionlistalter;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.time.format.DecimalStyle;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FoodListActivity extends AppCompatActivity {
 
     private RecyclerView mAllFoodRecyclerView;
     private RecyclerView mShoppingListRecyclerView;
-    private List<Food> mAllFoods;
-    private List<Food> mShoppingListFoods;
+    private AllFoodAdapter mAllFoodsAdapter;
+    private ShoppingListFoodAdapter mShoppingListAdapter;
+    private static List<Food> mAllFoods;
+    private static List<Food> mShoppingListFoods;
+    private boolean mIsShoppingListShown;
+    private FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
 
+        mAllFoods = new ArrayList<>();
+        mShoppingListFoods = new ArrayList<>();
+
+        mAllFoodRecyclerView = (RecyclerView) findViewById(R.id.all_food_list);
+        mShoppingListRecyclerView = (RecyclerView) findViewById(R.id.shoppling_list);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+        mAllFoodRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mShoppingListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         List<Food> foodStore = FoodStore.get(FoodListActivity.this).getFoods();
-        for(Food food:foodStore){
-            mAllFoods.add(food);
+
+        for (int ctr = 0; ctr < foodStore.size(); ctr++) {
+            mAllFoods.add(foodStore.get(ctr));
         }
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mIsShoppingListShown = !mIsShoppingListShown;
+                updateUI();
+            }
+        });
+
+        updateUI();
     }
 
-    private class FoodHolder extends RecyclerView.ViewHolder
-    implements View.OnClickListener{
+    private void updateUI() {
+        if (mIsShoppingListShown) {
+            mShoppingListAdapter = new ShoppingListFoodAdapter(mShoppingListFoods);
+            mShoppingListRecyclerView.setAdapter(mShoppingListAdapter);
+            mAllFoodRecyclerView.setVisibility(View.INVISIBLE);
+            mShoppingListRecyclerView.setVisibility(View.VISIBLE);
+            return;
+        }
+        mAllFoodsAdapter = new AllFoodAdapter(mAllFoods);
+        mAllFoodRecyclerView.setAdapter(mAllFoodsAdapter);
+        mShoppingListRecyclerView.setVisibility(View.INVISIBLE);
+        mAllFoodRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private abstract class FoodHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener {
 
         private Food mItemFood;
         private TextView mItemCat;
@@ -40,42 +88,135 @@ public class FoodListActivity extends AppCompatActivity {
                     parent,
                     false));
 
+            Log.w("tmp", "Food holder created.");
+
             itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+
 
             mItemCat = (TextView) itemView.findViewById(R.id.item_cat);
             mItemName = (TextView) itemView.findViewById(R.id.item_name);
         }
 
-        public void bind(Food food){
+        public void bind(Food food) {
             mItemFood = food;
 
-            mItemCat.setText(mItemFood.getCat().substring(0,1));
+            mItemCat.setText(mItemFood.getCat().substring(0, 1));
             mItemName.setText(mItemFood.getName());
         }
 
         @Override
         public void onClick(View view) {
-
+            Intent intent = FoodDetailActivity.newIntent(getApplicationContext(), mItemFood.getName());
+            startActivity(intent);
         }
+
+
+        @Override
+        public abstract boolean onLongClick(View view) ;
     }
 
-    private class FoodAdapter extends  RecyclerView.Adapter<FoodHolder>{
+    private class AllFoodHolder extends FoodHolder{
+
+        public AllFoodHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater, parent);
+        }
+
+        @Override
+        public boolean onLongClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "删除" + super.mItemFood.getName(), Toast.LENGTH_SHORT).show();
+                mAllFoods.remove(super.mItemFood);
+                updateUI();
+
+                return true;
+            }
+
+    }
+
+    private class ShoppingListFoodHolder extends FoodHolder{
+
+        public ShoppingListFoodHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater, parent);
+        }
+
+        @Override
+        public boolean onLongClick(View view)
+        {
+//            AlertDialog alertDialog = new AlertDialog()
+
+
+            mAllFoods.remove(super.mItemFood);
+            updateUI();
+
+            return true;
+        }
+
+    }
+
+    private class AllFoodAdapter extends RecyclerView.Adapter<AllFoodHolder> {
 
         private List<Food> mFoods;
 
-        @Override
-        public FoodHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+        public AllFoodAdapter(List<Food> foods) {
+
+            mFoods = foods;
         }
 
         @Override
-        public void onBindViewHolder(FoodHolder holder, int position) {
+        public AllFoodHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
 
+            return new AllFoodHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(AllFoodHolder holder, int position) {
+            Food food = mFoods.get(position);
+            holder.bind(food);
         }
 
         @Override
         public int getItemCount() {
             return mFoods.size();
         }
+    }
+
+    private class ShoppingListFoodAdapter extends RecyclerView.Adapter<ShoppingListFoodHolder> {
+
+        private List<Food> mFoods;
+
+        public ShoppingListFoodAdapter(List<Food> foods) {
+
+            mFoods = foods;
+        }
+
+        @Override
+        public ShoppingListFoodHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+
+            return new ShoppingListFoodHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ShoppingListFoodHolder holder, int position) {
+            Food food = mFoods.get(position);
+            holder.bind(food);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mFoods.size();
+        }
+    }
+
+    public static boolean addFoodToCart(String foodName) {
+        //What should be the context here? : new FoodListActivity() / getApplicationContext()
+        if (mShoppingListFoods.contains(FoodStore.get(new FoodListActivity()).getFood(foodName))) {
+            return false;
+        }
+
+        mShoppingListFoods.add(FoodStore.get(new FoodListActivity()).getFood(foodName));
+        return true;
     }
 }
